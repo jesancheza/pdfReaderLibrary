d685a6082447838e1dc67e095c92313ce1751f82
José Enrique Sanchez <jesadt69@gmail.com>
2015-03-28 11:05:41 +0100

    Initial Commit

---
+++ pdfReaderLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pdfReaderLibrary
+//
+//  Created by José Enrique Sanchez on 28/3/15.
+//  Copyright (c) 2015 Devappify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pdfReaderLibrary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pdfReaderLibrary
+//
+//  Created by José Enrique Sanchez on 28/3/15.
+//  Copyright (c) 2015 Devappify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
